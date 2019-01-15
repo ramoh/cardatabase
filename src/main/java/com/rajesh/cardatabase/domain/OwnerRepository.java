8d2d7bb017342aca8b176cf0233cb632f5e3ad85
ramoh <mohantyrajesh1984@gmail.com>
2019-01-15 13:12:06 +0530

    text

--- src/main/java/com/rajesh/cardatabase/domain/OwnerRepository.java
+++ src/main/java/com/rajesh/cardatabase/domain/OwnerRepository.java
@@ -2,7 +2,7 @@ package com.rajesh.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-//this is a test comment
+//this is a test comment asdasd
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 }
